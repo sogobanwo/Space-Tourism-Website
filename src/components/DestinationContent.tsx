6e36643082910fcd402cb0a0e1146336327c1dd4
sogobanwo <sogobanwo@gmail.com>
2023-04-02 22:57:55 +0100

    Feat: Added the destination page with its functionalities

--- src/components/DestinationContent.tsx
+++ src/components/DestinationContent.tsx
@@ -1,11 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Mars from "../imageAsset/destination/image-mars.png";
-import { getDestinationData } from "../helpers/services";
+import Moon from "../imageAsset/destination/image-moon.png";
+import Europa from "../imageAsset/destination/image-europa.png";
+import Titan from "../imageAsset/destination/image-titan.png";
+import data from "../data.json";
 
 type Props = {};
+type dest = "Moon" | "Mars" | "Europa" | "Titan";
+type destiny = {
+  name: string;
+  images: Record<string, string>;
+  description: string;
+  distance: string;
+  travel: string;
+};
 
 const DestinationContent = (props: Props) => {
-  const { name, images, description, distance, travel } = destinations;
+  const notActiveDestination = "text-center text-xl text-slate-300 font-robotoCondensed mr-4"
+  const activeDestination = "text-center text-xl text-slate-300 font-robotoCondensed border-b-2 mr-4"
+  const destinations: destiny[] = data.destinations;
+  const [selectedDestination, setSelectedDestination] = useState<dest>("Mars");
+  const oneDestination = destinations.find(
+    (destination: destiny) => destination.name === selectedDestination
+  )!;
+
+  const { name, description, distance, travel } = oneDestination;
   return (
     <div className="flex justify-center mt-12">
       <div className="flex flex-col lg:w-[50%]">
@@ -17,73 +36,75 @@ const DestinationContent = (props: Props) => {
         </h1>
         <div className="flex items-center justify-center">
           <img
-            src={Mars}
-            alt="Mars"
+            src={
+              selectedDestination === "Moon"
+                ? Moon
+                : selectedDestination === "Mars"
+                ? Mars
+                : selectedDestination === "Europa"
+                ? Europa
+                : Titan
+            }
+            alt={selectedDestination}
             className="flex items-center mt-8 lg:w-[60%]"
           />
         </div>
       </div>
       <div className="lg:w-[50%] lg:mt-20">
         <div className="flex">
-          <h1 className="text-center text-xl text-slate-300 font-robotoCondensed mr-4">
+          <h1
+            className={selectedDestination === "Moon" ? activeDestination : notActiveDestination}
+            onClick={() => {
+              setSelectedDestination("Moon");
+            }}
+          >
             MOON
           </h1>
-          <h1 className="text-center text-xl text-slate-300 font-robotoCondensed border-b-2 mr-4">
+          <h1
+            className={selectedDestination === "Mars" ? activeDestination : notActiveDestination}
+            onClick={() => {
+              setSelectedDestination("Mars");
+            }}
+          >
             MARS
           </h1>
-          <h1 className="text-center text-xl text-slate-300 font-robotoCondensed mr-4">
+          <h1
+            className={selectedDestination === "Europa" ? activeDestination : notActiveDestination}
+            onClick={() => {
+              setSelectedDestination("Europa");
+            }}
+          >
             EUROPA
           </h1>
-          <h1 className="text-center text-xl text-slate-300 font-robotoCondensed mr-4">
+          <h1
+            className={selectedDestination === "Titan" ? activeDestination : notActiveDestination}
+            onClick={() => {
+              setSelectedDestination("Titan");
+            }}
+          >
             TITAN
           </h1>
         </div>
         <h1 className="font-serif text-4xl leading-tight md:text-7xl font-light text-slate-300 my-8">
-          MARS
+          {name}
         </h1>
         <p className="lg:w-[70%] text-xl text-slate-300 font-robotoCondensed mb-12">
-          Don’t forget to pack your hiking boots. You’ll need them to tackle
-          Olympus Mons, the tallest planetary mountain in our solar system. It’s
-          two and a half times the size of Everest!
+          {description}
         </p>
         <hr className="lg:w-[75%] text-slate-300 mb-8" />
         <div className="flex text-slate-300 font-robotoCondensed lg:w-[50%] justify-between items-center">
           <div>
             <p>AVG. DISTANCE</p>
-            <h1 className="text-3xl">225 MIL. KM</h1>
+            <h1 className="text-3xl">{distance}</h1>
           </div>
           <div>
             <p>EST. TRAVEL TIME</p>
-            <h1 className="text-3xl">9 MONTHS</h1>
+            <h1 className="text-3xl">{travel}</h1>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
-type dest = "Moon" | "Mars" | "Europa" | "Titan"
-
-const DestinationSelected = () =>{
-  const [destinations, setDestinations] = useState([]);
-  const [selectedDestination, setSelectedDestination] = useState<dest>("Moon");
-  useEffect(() => {
-    const fetchData = async () => {
-      setDestinations(await getDestinationData());
-      console.log(destinations);
-    };
-    fetchData();
-    const OneDestination = destinations.find((destination:{})=> {destination.name == selectedDestination})
-  }, []);
-
-
-
-  const handleClick = (destination: string) => {
-      setSelectedDestination(destination)
-  };
-  return(
-
-  )
-}
 
 export default DestinationContent;
